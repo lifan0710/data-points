d61495f5da48b8947947f4d44a213e9dbf34fdb5
jiangseventeen <jiangseventeen@gmail.com>
2018-04-02 17:28:17 +0800

    添加公共样式文件

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import index from './Index'
+
+import './assets/scss/normalize.scss'
+import './assets/scss/common.scss'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  components: { index },
+  template: '<Index/>'
 })
