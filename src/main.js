b7da065fb5e87b709e614ecb5fa93e0c960a216a
jiangseventeen <jiangseventeen@gmail.com>
2018-04-28 21:31:58 +0800

    添加font-awesome图标库

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import index from './Index'
+import store from './store'
 
+import ElementUI from 'element-ui'
+import 'font-awesome/css/font-awesome.css'
 import 'normalize.css'
+
 import './assets/scss/common.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { index },
-  template: '<index/>'
+  store,
+  template: '<index/>',
+  components: { index }
 })
