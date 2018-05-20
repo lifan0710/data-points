2dbaccca3e2843e67bd0814f23355578a15b7ad8
jiangseventeen <jiangseventeen@gmail.com>
2018-05-21 07:37:51 +0800

    修改顶部菜单栏

--- src/Index.vue
+++ src/Index.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
+    <the-heading></the-heading>
+    <the-leftside-layer></the-leftside-layer>
+    <the-rightside-setting></the-rightside-setting>
   </div>
 </template>
 
 <script>
+import theHeading from './components/header/TheHeading'
+import theLeftsideLayer from './components/leftside/TheLeftsideLayer'
+import theRightsideSetting from './components/rightside/TheRightsideSetting'
+
 export default {
   name: 'index',
   data () {
     return {}
+  },
+  components: {
+    theHeading,
+    theLeftsideLayer,
+    theRightsideSetting
   }
 }
-</script> 
+</script>
 
 <style lang="scss" scoped>
 #app {
   height: 100%;
+  background: url(./assets/img/main-bg.png) repeat;
+  background-size: 8px 8px;
 }
 </style>
---
+++ src/components/header/TheHeading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dp-heading"></div>
+</template>
+
+<style lang="scss" scoped>
+.dp-heading {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  height: 60px;
+  background-color: #333;
+}
+</style>
