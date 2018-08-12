f4a3aea45e3456d1d9213317439d6075480df03c
jiangseventeen <jiangseventeen@gmail.com>
2018-08-12 23:20:35 +0800

    store添加获取组件菜单action

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,46 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import legends from '../api/legends'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    // 折叠/显示左侧面板
+    // 是否折叠左侧面板
     isCollapseLeftside: false,
-    // 折叠/显示右侧面板
+    // 是否折叠右侧面板
     isCollapseRightside: false,
     // 画布中渲染组件列表
     inRenderComponentList: [],
     // 已选中组件列表
     currentComponentList: [],
     // 画布设置选项
-    pageSetting: {}
+    pageSetting: {},
+    // 页面缩放比例
+    pageScale: 0,
+    // 顶部组件菜单
+    legendMenu: []
   },
   mutations: {
+    // 折叠/显示左侧面板
     toggleLeftside: state => {
       state.isCollapseLeftside = !state.isCollapseLeftside
     },
+    // 折叠/显示右侧面板
     toggleRightside: state => {
       state.isCollapseRightside = !state.isCollapseRightside
+    },
+    // 设置顶部组件菜单
+    setLegendMenu: (state, legends) => {
+      state.legendMenu = legends
     }
   },
-  actions: {}
+  actions: {
+    // 获取顶部组件菜单数据
+    getLegendsMenu ({ commit }) {
+      legends.getLegendsMenu(legends => {
+        commit('setLegendMenu', legends)
+      })
+    }
+  }
 })
